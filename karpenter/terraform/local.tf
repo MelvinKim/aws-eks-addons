7fce915513fdf4c07b354667adeee7a7d3030266
Melvin Kimathi <karutikimathi@MKIMATHI-ENG.local>
2024-06-04 08:51:41 +0300

    setup karpenter on AWS EKS

---
+++ karpenter/terraform/local.tf
@@ -0,0 +1,6 @@
+locals {
+  region = "us-east-1"
+  env = "stage"
+  cluster_name = "karpenter-demo"
+  cluster_version = "1.30"
+}
---
+++ karpenter/terraform/variable.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  type = string
+  description = "The cluster name"
+}
+
+variable "region" {
+  type = string
+  description = "AWS region which will hos the EKS cluster"
+}
